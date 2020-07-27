e6ef0cce8c1e5aa485b400ba7a912d51c2f5715a
Diez B. Roggisch <deets@web.de>
2020-07-27 19:08:14 +0200

    Introduce TkGpioZeroBridge and an example how to use it
    This also begins the implementation of a very simple mocked
    gpiozero factory.

---
+++ gpio-utils/tkgpiozero.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# Copyright: 2020, Diez B. Roggisch, Berlin . All rights reserved.
+import time
+import threading
+import gpiozero.pins
+
+
+class Pin(gpiozero.pins.Pin):
+    def __init__(self, factory, number):
+        super().__init__()
+        self._factory = factory
+        self.number = number
+        self._pull = "floating"
+        self._edges = "none"
+        self._when_changed = None
+        self._state = 0
+
+    def _get_state(self):
+        return self._state
+
+    def _set_state(self, value):
+        if value == self._state:
+            return
+        edge = "rising" if value and not self._state else "falling"
+        self._state = value
+        if self._edges in ("both", edge) and self._when_changed is not None:
+            self._when_changed(self._factory.ticks(), value)
+
+    def _get_pull(self):
+        return self._pull
+
+    def _set_pull(self, value):
+        self._pull = value
+
+    def _get_edges(self):
+        return self._edges
+
+    def _set_edges(self, value):
+        self._edges = value
+
+    def _get_when_changed(self):
+        return self._when_changed
+
+    def _set_when_changed(self, value):
+        self._when_changed = value
+
+
+class PinFactory(gpiozero.pins.Factory):
+
+    def __init__(self, *a, **k):
+        super().__init__(*a, **k)
+        self._pins = {}
+        self._periodic_pins = []
+        t = threading.Thread(target=self._drive_periodic_pins)
+        t.daemon = True
+        t.start()
+
+    def pin(self, spec):
+        if spec not in self._pins:
+            self._pins[spec] = Pin(self, spec)
+        return self._pins[spec]
+
+    def ticks(self):
+        return time.monotonic()
+
+    def ticks_diff(self, later, earlier):
+        return later - earlier
+
+    def toggle_periodic(self, spec, timeout):
+        self._periodic_pins.append((spec, timeout))
+
+    def _drive_periodic_pins(self):
+        while True:
+            for spec, timeout in self._periodic_pins:
+                time.sleep(timeout)
+                if spec in self._pins:
+                    self._pins[spec].state = not self._pins[spec].state
---
+++ gpio-utils/tkinter-gpio-mainloop-integration.py
@@ -0,0 +1,72 @@
+import os
+import time
+from functools import partial
+import tkinter as tk
+from gpiozero import Device, Button, LED
+from collections import deque
+
+INPUT_A = 10
+INPUT_B = 11
+
+# This is only for mocking things on a PC!
+# The Bridge works without the following
+# lines for a real gpiozero
+from tkgpiozero import PinFactory
+Device.pin_factory = PinFactory()
+Device.pin_factory.toggle_periodic(INPUT_A, 1.0)
+
+
+class TkGpioZeroBridge:
+
+    def __init__(self, root):
+        self._root = root
+        read_fd, self._write_fd = os.pipe()
+        root.createfilehandler(read_fd, tk.READABLE, self._event_callback)
+        self._button_events = deque()
+
+    def register_button(self, button, callback):
+        button.when_pressed = partial(self._button_callback, callback)
+
+    def _button_callback(self, callback):
+        self._button_events.append(callback)
+        # just signal the mainloop!
+        os.write(self._write_fd, b"!")
+
+    def _event_callback(self, fd, _mask):
+        # we always know there is just one byte
+        # in there, we just consume that away as
+        # the purpose of this is to wake us up.
+        os.read(fd, 100)
+        try:
+            while True:
+                # invoke our callbacks
+                self._button_events.pop()()
+        except IndexError:
+            pass
+
+
+class Application:
+
+    def __init__(self, root):
+        label = tk.Label(master=root, text="Threadmagie hier drunter:")
+        label.pack()
+        self._messages_from_automaton = tk.Label(master=root, text="")
+        self._messages_from_automaton.pack()
+        self._start = time.monotonic()
+
+    def set_text(self, text):
+        self._messages_from_automaton["text"] = text
+
+
+def main():
+    root = tk.Tk()
+    bridge = TkGpioZeroBridge(root)
+    button_a = Button(INPUT_A)
+    button_b = Button(INPUT_B)
+    app = Application(root)
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
