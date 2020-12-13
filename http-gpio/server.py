a951afe0278c1867121aa8d1c2215f2a066926b7
Diez B. Roggisch <deets@web.de>
2020-12-13 15:13:12 +0100

    Brought forward to tornado 6

--- http-gpio/server.py
+++ http-gpio/server.py
@@ -2,6 +2,7 @@ import os
 import threading
 import time
 from functools import partial
+import logging
 
 import tornado.ioloop
 import tornado.web
@@ -13,7 +14,6 @@ define("port", default=12345, help="run on the given port", type=int)
 
 
 class IndexHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
     def get(self):
         self.render("index.html")
 
@@ -24,22 +24,18 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     def open(self, *args):
         self.id = self.get_argument("id")
-        self.stream.set_nodelay(True)
         self.CLIENTS[self.id] = {
             "id": self.id,
             "object": self,
             "write_message": self.write_message,
         }
 
-
     def on_message(self, message):
         pass
-
 
     def on_close(self):
         if self.id in self.CLIENTS:
             del self.CLIENTS[self.id]
-
 
     @classmethod
     def dispatch(cls, value):
@@ -47,10 +43,8 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
             client["write_message"](repr(value))
 
 
-def gpio_loop():
-    ioloop = tornado.ioloop.IOLoop.current()
+def gpio_loop(ioloop):
     value = True
-
     while True:
         time.sleep(1.0)
         value = not value
@@ -59,6 +53,10 @@ def gpio_loop():
 
 def main():
     parse_command_line()
+
+    logging.basicConfig(
+        level=logging.INFO,
+    )
 
     settings = {
         "static_path": os.path.join(os.path.dirname(__file__), "static"),
@@ -71,11 +69,13 @@ def main():
         ],
         **settings
     )
-    t = threading.Thread(target=gpio_loop)
-    t.setDaemon(True)
-    t.start()
     app.listen(options.port)
     ioloop = tornado.ioloop.IOLoop.instance()
+
+    t = threading.Thread(target=gpio_loop, args=(ioloop,))
+    t.daemon = True
+    t.start()
+
     ioloop.start()
 
 
